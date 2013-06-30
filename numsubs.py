cb7adcd493f71337b21fa0093fccbbe5f63a681d
Yee Mey Seah <seahym@gmail.com>
2013-06-30 00:20:54 -0400

    comments on script for counting changes

---
+++ numsubs.py
@@ -0,0 +1,8 @@
+# script to count number of changes
+# input seq_pars.log file
+# first get outgroup name (str)
+# search for <nt outgroup> and replace with <*****nt outgroup>
+# save changes in input file
+# now count number of "nt ==> nt" and "nt --> nt"
+# print output number of nt changes to numsubs.txt
+
