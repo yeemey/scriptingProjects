9c70e0bb9825b41b0115252b784deff852eab5a5
Yee Mey Seah <seahym@gmail.com>
2015-11-18 19:05:56 -0800

    Renamed variables for clarity and use of camelCase.

--- python/PEDvPUDplot.py
+++ python/PEDvPUDplot.py
@@ -12,165 +12,165 @@ import numpy as np
 import matplotlib.pyplot as plt
 import itertools
 
-filepath = raw_input("Enter path to SDT matrix file: ")
-filepath2 = raw_input("Enter path to TreePuzzle distance output file: ")
+SdtFilepath = raw_input("Enter path to SDT matrix file: ")
+TpzFilepath = raw_input("Enter path to TreePuzzle distance output file: ")
 
-def getSdtId(filepath):
+def getSdtId(SdtFilepath):
     """
     Reads SDT 1.0 matrix output (filename_mat.txt) and returns list of sequence IDs.
     
-    ::param filepath::  Path to SDT file (input).
-    ::param sdtfile::   Input file.
-    ::param seqid::     List of sequence ID strings.
-    ::param eachseqid:: Individual sequence ID string.
-    ::param line::      Line from input file.
-    ::param eachline::  List of elements from param line.
+    ::param SdtFilepath::    Path to SDT file (input).
+    ::param SdtFile::        Input file.
+    ::param sequenceIds::    List of sequence ID strings.
+    ::param eachSequenceId:: Individual sequence ID string.
+    ::param line::           Line from input file.
+    ::param splitLine::      List of elements from param line.
     """
-    with open(filepath) as sdtfile:
-        seqid = []
-        eachseqid = ""
-        for line in sdtfile:
-            eachline = line.split(',')
-            eachseqid = eachline[0]
-            if len(eachseqid) > 10:
-                eachseqid = eachseqid[:10]
-                seqid.append(eachseqid)
+    with open(SdtFilepath) as SdtFile:
+        sequenceIds = []
+        eachSequenceId = ""
+        for line in SdtFile:
+            splitLine = line.split(',')
+            eachSequenceId = splitLine[0]
+            if len(eachSequenceId) > 10:
+                eachSequenceId = eachSequenceId[:10]
+                sequenceIds.append(eachSequenceId)
             else:
-                seqid.append(eachseqid)
-    return seqid
+                sequenceIds.append(eachSequenceId)
+    return sequenceIds
 
-def getSdtDist(filepath):
+def getSdtDist(SdtFilepath):
     """
     Reads SDT 1.0 matrix output (filename_mat.txt) and returns list of pairwise distances as float numbers. 
     
     Order of list elements corresponds to order of distances (left to right) in each matrix row (top to bottom) in the SDT file. Pairwise identities of 0 between the same pairwise sequences are excluded. Pairwise distances are calculated as the complement of identities.
     
-    ::param filepath::     Path to SDT file (input).
-    ::param sdtsims::      Input file.
-    ::param sdtdist::      List of pairwise distances (float).
+    ::param SdtFilepath::  Path to SDT file (input).
+    ::param SdtFile::      Input file.
+    ::param SdtDistances:: List of pairwise distances (float).
     ::param line::         Line from input file.
-    ::param simsline::     List of elements from param line.
+    ::param splitLine::    List of elements from param line.
     ::param similarities:: Slice of param simsline to exclude sequence ID & distance of 0 (pairwie comparison with self).
     ::param item::         Individual distance measure.
     """
-    with open(filepath) as sdtsims:
-        sdtdist = []
-        for line in sdtsims:
-            simsline = line.split(',')
-            similarities = simsline[1:(len(simsline)-1)]
+    with open(SdtFilepath) as SdtFile:
+        SdtDistances = []
+        for line in SdtFile:
+            splitLine = line.split(',')
+            similarities = splitLine[1:(len(splitLine)-1)]
             for item in similarities:
                 item = item.strip('\n')
                 item = float(item)
-                sdtdist.append(1-item)
-    return sdtdist    
+                SdtDistances.append(1-item)
+    return SdtDistances    
 
-def getPud(filepath):
+def getPud(SdtFilepath):
     """
     Calls functions getSDTid and getSDTdist and returns dictionary of pairwise sequence (tuple) keys and pairwise uncorrected distance values.
     
-    ::param filepath::     Path to SDT file (input).
-    ::param SDTid::       List of sequence ID strings.
-    ::param SDTdist::     List of dictionary values (float distances).
-    ::param seqpairs::    List of dictionary keys (tuple of sequence IDs).
-    ::param seq1Count::   Var to keep count of 1st sequence ID in pair.
-    ::param seq2Count::   Var to keep count of 2nd sequence ID in pair.
-    ::param eachseqpair:: Tuple of sequence IDs
-    ::param PUDS::        Dictionary of pairwise sequence keys and uncorrected distance values, excluding pairwise comparisons of sequence with itself.
+    ::param SdtFilepath::      Path to SDT file (input).
+    ::param SdtIds::           List of sequence ID strings.
+    ::param SdtDistances::     List of dictionary values (float distances).
+    ::param sequencePairs::    List of dictionary keys (tuple of sequence IDs).
+    ::param sequence1Count::   Var to keep count of 1st sequence ID in pair.
+    ::param sequence2Count::   Var to keep count of 2nd sequence ID in pair.
+    ::param eachSequencePair:: Tuple of sequence IDs
+    ::param Puds::             Dictionary of pairwise sequence keys and uncorrected distance values, excluding pairwise comparisons of sequence with itself.
     """
-    SDTid = getSdtId(filepath)
-    SDTdist = getSdtDist(filepath) 
-    seqpairs = [] 
-    seq1Count = 1
-    while seq1Count<len(SDTid):
-        seq2Count = 0
-        while seq2Count < seq1Count:
-            eachseqpair = (SDTid[seq1Count], SDTid[seq2Count])
-            seqpairs.append(eachseqpair)
-            seq2Count += 1
-        seq1Count += 1
-    PUDS = dict(itertools.izip(seqpairs, SDTdist))
-    return PUDS
+    SdtIds = getSdtId(SdtFilepath)
+    SdtDistances = getSdtDist(SdtFilepath) 
+    sequencePairs = [] 
+    sequence1Count = 1
+    while sequence1Count<len(SdtIds):
+        sequence2Count = 0
+        while sequence2Count < sequence1Count:
+            eachSequencePair = (SdtIds[sequence1Count], SdtIds[sequence2Count])
+            sequencePairs.append(eachSequencePair)
+            sequence2Count += 1
+        sequence1Count += 1
+    Puds = dict(itertools.izip(sequencePairs, SdtDistances))
+    return Puds
 
-def getTpzIdDist(filepath2):
+def getTpzIdDist(TpzFilepath):
     """
     Reads Tree-Puzzle 5.2 maximum likelihood distance file (outdist.txt) and returns list of lists comprising sequence id (first element) and all pairwise distances.
     
-    ::param filepath2::      Path to Tree-Puzzle file (input).
-    ::param tpzfile::        Input file.
-    ::param numseq::         Number of sequences in file.
+    ::param TpzFilepath::      Path to Tree-Puzzle file (input).
+    ::param TpzFile::        Input file.
+    ::param numberSequences::         Number of sequences in file.
     ::param line::           Line from input file.
-    ::param eachline2::      List of elements from param line.
+    ::param splitLine::      List of elements from param line.
     ::param item::           Each element in param eachline2.
-    ::param eachline2clean:: List of elements from param line, minus empty strings.
-    ::param seqid2element:: List of sequence ID string (first element) and pairwise distances for that sequence (subsequent elements).
-    ::param seqid2::        List of param seqid2element.    
+    ::param splitLinesClean:: List of elements from param line, minus empty strings.
+    ::param eachSequenceIdDistance:: List of sequence ID string (first element) and pairwise distances for that sequence (subsequent elements).
+    ::param sequenceIdDistances::        List of param eachSequenceId.    
     """
-    with open(filepath2) as tpzfile:
-        numseq = tpzfile.readline()
-        numseq = numseq.strip('\n')
-        numseq = int(numseq)
-        seqid2 = []
-        seqid2element = []
-        eachline2clean = []
-        for line in tpzfile:
-            eachline2 = line.split(' ')
-            for item in eachline2:
+    with open(TpzFilepath) as TpzFile:
+        numberSequences = TpzFile.readline()
+        numberSequences = numberSequences.strip('\n')
+        numberSequences = int(numberSequences)
+        sequenceIdDistances = []
+        eachSequenceIdDistance = []
+        splitLinesClean = []
+        for line in TpzFile:
+            splitLine = line.split(' ')
+            for item in splitLine:
                 item = item.strip('\n')
                 if item != '':
-                    eachline2clean.append(item)
-        while len(seqid2) < numseq:
-            seqid2element = eachline2clean[:numseq+1] 
-            seqid2.append(seqid2element)
-            del eachline2clean[:numseq+1]
-    return seqid2
+                    splitLinesClean.append(item)
+        while len(sequenceIdDistances) < numberSequences:
+            eachSequenceIdDistance = splitLinesClean[:numberSequences+1] 
+            sequenceIdDistances.append(eachSequenceIdDistance)
+            del splitLinesClean[:numberSequences+1]
+    return sequenceIdDistances
 
-def getPed(filepath2):
+def getPed(TpzFilepath):
     """
     Calls the function getTPZiddist and returns dictionary of pairwise sequence (tuple) keys and pairwise maximum likelihood distance values. 
     
-    ::param filepath2::      Path to Tree-Puzzle file (input).
-    ::param TPZiddist::      List of lists of sequence ID string (first element) and distances for that sequence (subsequent elements).
-    ::param TPZdistperseq::  List of param distanceList.
-    ::param distanceList::   List of distances for one sequence.
+    ::param TpzFilepath::      Path to Tree-Puzzle file (input).
+    ::param TpzIdDistances::      List of lists of sequence ID string (first element) and distances for that sequence (subsequent elements).
+    ::param TpzDistancesPerSequence::  List of param distanceList.
+    ::param eachDistanceList::   List of distances for one sequence.
     ::param TPZdistances::   List of dictionary values (float distances for all sequences.
-    ::param seq1Count::      Var to keep count of 1st sequence ID in pair.
-    ::param seq2Count::      Var to keep count of 2nd sequence ID in pair.
-    ::param eachseqpair2::   Tuple of sequence string pairs.
-    ::param seqpairs2::      List of dictionary keys (param eachseqpair2).
+    ::param sequence1Count::      Var to keep count of 1st sequence ID in pair.
+    ::param sequence2Count::      Var to keep count of 2nd sequence ID in pair.
+    ::param eachSequencePair::   Tuple of sequence string pairs.
+    ::param sequencePairs::      List of dictionary keys (param eachseqpair2).
     ::param entries::        List of sequence ID string (first element) and distances for that sequence (subsequent elements).
     ::param countDistances:: Var to keep count of distance values per sequence.
     ::param PEDSinit::       Dictionary of pairwise sequence keys and evolutionary distance values.
     ::param PEDS::           Dictionary of pairwise sequence keys and evolutionary distance values, excluding pairwise comparisons of sequence with itself.
     """
-    TPZiddist = getTpzIdDist(filepath2)
-    TPZdistperseq = [] 
-    TPZdistances = []
-    seqpairs2 = []
-    seq1Count = 0
-    while seq1Count < len(TPZiddist):
-        seq2Count = 0
-        while seq2Count < len(TPZiddist):
-            eachseqpair2 = (TPZiddist[seq1Count][0], TPZiddist[seq2Count][0])
-            seqpairs2.append(eachseqpair2)
-            seq2Count += 1
-        seq1Count += 1
-    for entries in TPZiddist:
-        TPZdistperseq.append(entries[1:])
-    for distanceList in TPZdistperseq:
+    TpzIdDistances = getTpzIdDist(TpzFilepath)
+    TpzDistanceListPerSequence = [] 
+    TpzAllDistances = []
+    sequencePairs = []
+    sequence1Count = 0
+    while sequence1Count < len(TpzIdDistances):
+        sequence2Count = 0
+        while sequence2Count < len(TpzIdDistances):
+            eachSequencePair = (TpzIdDistances[sequence1Count][0], TpzIdDistances[sequence2Count][0])
+            sequencePairs.append(eachSequencePair)
+            sequence2Count += 1
+        sequence1Count += 1
+    for entries in TpzIdDistances:
+        TpzDistanceListPerSequence.append(entries[1:])
+    for eachDistanceList in TpzDistanceListPerSequence:
         countDistances = 0
-        while countDistances < len(distanceList):
-            distanceList[countDistances] = float(distanceList[countDistances])
+        while countDistances < len(eachDistanceList):
+            eachDistanceList[countDistances] = float(eachDistanceList[countDistances])
             countDistances += 1
-        TPZdistances = TPZdistances + distanceList
+        TpzAllDistances = TpzAllDistances + eachDistanceList
     
-    PEDSinit = dict(itertools.izip(seqpairs2, TPZdistances))
-    PEDS = {}
-    for key,value in PEDSinit.iteritems():
+    PedsInit = dict(itertools.izip(sequencePairs, TpzAllDistances))
+    Peds = {}
+    for key,value in PedsInit.iteritems():
         if key[0] != key[1]:
-            PEDS[key] = value
-    return PEDS
+            Peds[key] = value
+    return Peds
 
-def PudXPedY(filepath, filepath2):
+def PudXPedY(SdtFilepath, TpzFilepath):
     """
     Calls the functions getPUD and getPED and returns distance values for corresponding keys in both dictionaries, as two lists packed in a tuple.
     
@@ -181,16 +181,16 @@ def PudXPedY(filepath, filepath2):
     ::param x::         List of evolutionary distances.
     ::param y::         List of uncorrected distances.
     """
-    PUDdict = getPud(filepath)
-    PEDdict = getPed(filepath2)
+    PudDict = getPud(SdtFilepath)
+    PedDict = getPed(TpzFilepath)
     x = []
     y = []
-    for key in PUDdict.keys():
-        y.append(PUDdict[key])
-        x.append(PEDdict[key])
+    for key in PudDict.keys():
+        y.append(PudDict[key])
+        x.append(PedDict[key])
     return (x,y)
 
-def reportDistances(filepath, filepath2):
+def reportDistances(SdtFilepath, TpzFilepath):
     """
     Prints sequence pairs and distances (uncorrected and evolutionary) to file.
     
@@ -200,20 +200,20 @@ def reportDistances(filepath, filepath2):
     ::param PEDdict::   Dictionary of pairwise sequence keys and evolutionary distance values, excluding pairwise comparisons of sequence with itself.
     ::param report::    Function output written to file.        
     """
-    PUDdict = getPud(filepath)
-    PEDdict = getPed(filepath2)
+    PudDict = getPud(SdtFilepath)
+    PedDict = getPed(TpzFilepath)
     with open("PUDPED.txt", "w") as report:
         report.write("sequencePair, uncorrectedDistance, evolutionaryDistance \n")
-        for key in PUDdict.keys():
-            report.write(str(key) + ", " + str(PUDdict[key]) + ", " + str(PEDdict[key]) + "\n")
+        for key in PudDict.keys():
+            report.write(str(key) + ", " + str(PudDict[key]) + ", " + str(PedDict[key]) + "\n")
     return report
 
 # Output pairwise distances
-reportDistances(filepath, filepath2)
+reportDistances(SdtFilepath, TpzFilepath)
 print "Pairwise distances printed to PUDPED.txt"
 
 # Output scatterplot of pairwise distances
-scatterPoints = PudXPedY(filepath, filepath2)
+scatterPoints = PudXPedY(SdtFilepath, TpzFilepath)
 plt.scatter(scatterPoints[0], scatterPoints[1])
 plt.xlabel('Pairwise evolutionary distance')
 plt.ylabel('Pairwise uncorrected distance')
